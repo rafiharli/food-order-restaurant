c8866e153e2418d60d9339b2f407736e1e52d401
rafiharli <135038628+rafiharli@users.noreply.github.com>
2023-12-07 10:29:58 +0700

    Create footer.php

---
+++ partials-front/footer.php
@@ -0,0 +1,32 @@
+
+    <!-- social Section Starts Here -->
+    <section class="social">
+      <div class="container text-center">
+        <ul>
+          <li>
+            <a href="https://instagram.com/rafiharlii?igshid=ZDc4ODBmNjlmNQ==" target="_blank"
+              ><img
+                src="https://img.icons8.com/fluent/48/000000/instagram-new.png"
+            /></a>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+      <div class="container text-center">
+        <p>All rights reserved. Designed By <a href="<?php echo SITEURL; ?>contact" target="_blank">Rafi Harlianto</a></p>
+        <br>
+        <p><a href="<?php SITEURL; ?>admin/login.php" target="_blank">Admin Page</a></p>
+        <br>
+      </div>
+      
+    </section>
+    <!-- footer Section Ends Here -->
+   
+    <script>document.addEventListener('contextmenu', event => event.preventDefault());</script>
+
+  </body>
+</html>
